d6184fa19690fe2bdbf7b944210a345f265ba941
petrosdiveris <petros@diveris.org>
2025-04-11 01:08:50 +0100

    feature/exhibitions: CS improvements

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,18 +1,19 @@
+@php use App\Models\Site; @endphp
 <footer class="footer">
     <div class="content has-text-centered">
         <p>
-            @if(\App\Models\Site::hasFooterMenu())
-                @foreach(\App\Models\Site::getFooterMenuItems() as $menuItem)
-                    <a class="is-size-7-mobile sofia" href="{{ url($menuItem->slug) }}">
-                        {{ $menuItem->translate($locale)->title ?? $menuItem->title }}
-                    </a> |
+            @if(Site::hasFooterMenu())
+                @foreach(Site::getFooterMenuItems() as $menuItem)
+                <a class="is-size-7-mobile sofia" href="{{ url($menuItem->slug) }}">
+                    {{ $menuItem->translate($locale)->title ?? $menuItem->title }}
+                </a> |
                 @endforeach
             @endif
             &nbsp;
             <a href=""><i class="fa-brands fa-pinterest-p"></i></a>&nbsp;
             <a href="https://www.facebook.com/groups/8160377530655543/">
                 <i class="fa-brands fa-facebook-f"></i>
-            </a>&nbsp;&nbsp;
+            </a>&nbsp;
             <a href=""><i class="fa-brands fa-twitter"></i></a>&nbsp;&nbsp;
             <a href=""><i class="fa-brands fa-instagram"></i></a>&nbsp;
         </p>
